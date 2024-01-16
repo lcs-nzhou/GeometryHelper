d9b74c1e790bb1c39722f098755ac658b00d26a7
Nina Zhou <nzhou@lcs.on.ca>
2024-01-16 12:04:50 -0500

    cone view

---
+++ GeometryHelper/Views/ConeView.swift
@@ -0,0 +1,85 @@
+//
+//  ConeView.swift
+//  GeometryHelper
+//
+//  Created by Shuyu Zhou on 2024-01-16.
+//
+
+import SwiftUI
+
+struct ConeView: View {
+    //MARK: Stored properties
+    @State var currentCone = cone(radius: 10.0, height: 20.0, slant: 20.0)
+    
+    //MARK: Computed properties
+    var body: some View {
+        VStack(alignment: .leading){
+            Image("ConeImage")
+                .resizable()
+                .frame(maxHeight: 300)
+            HStack {
+                Text("0")
+                    .padding(.leading)
+                Slider(value: $currentCone.radius, in: 0...100)
+                Text("100")
+                    .padding(.trailing)
+            }
+            HStack {
+                Text("0")
+                    .padding(.leading)
+                Slider(value: $currentCone.height, in: 0...100)
+                Text("100")
+                    .padding(.trailing)
+            }
+            HStack {
+                Text("0")
+                    .padding(.leading)
+                Slider(value: $currentCone.slant, in: 0...100)
+                Text("100")
+                    .padding(.trailing)
+            }
+            .padding(.bottom)
+            
+            Text("Radius")
+                .padding(.leading)
+                .bold()
+            Text("\(currentCone.radius)")
+                .padding(.bottom)
+                .padding(.leading)
+                
+            Text("Height")
+                .padding(.leading)
+                .bold()
+            Text("\(currentCone.height)")
+                .padding(.bottom)
+                .padding(.leading)
+                
+            Text("Slant")
+                .padding(.leading)
+                .bold()
+            Text("\(currentCone.slant)")
+                .padding(.bottom)
+                .padding(.leading)
+                
+            Text("Surface Area")
+                .padding(.leading)
+                .bold()
+            Text("\(currentCone.surfaceArea)")
+                .padding(.leading)
+                .padding(.bottom)
+            
+            Text("Volume")
+                .padding(.leading)
+                .bold()
+            Text("\(currentCone.volume)")
+                .padding(.leading)
+            
+            Spacer()
+        }
+        
+    }
+}
+
+#Preview {
+    ConeView()
+}
